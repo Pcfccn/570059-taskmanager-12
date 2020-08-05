23609f852bea7080f8ac2da5a9bc03b1bfc70235
Pcfccn <47412995+Pcfccn@users.noreply.github.com>
2020-08-05 15:31:26 +0300

    Revert "Module2 task1 dev"

--- src/view/board.js
+++
@@ -1,8 +0,0 @@
-const createBoardTemplate = () => {
-  return (`<section class="board container"></section>`);
-};
-const createBoardTasksTemplate = () => {
-  return (`<div class="board__tasks"></div>`);
-};
-
-export {createBoardTemplate, createBoardTasksTemplate};
